4dac4202901f93897e5382297ba0881d0bd3d804
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-05-10 12:21:01 -0400

    update kinase recovery plots

--- helpers.py
+++ helpers.py
@@ -242,7 +242,7 @@ def create_discussion(results: dict):
     for k in results:
         prompt += f"## {k}\n"
         prompt += results[k] + "\n"
-    system_prompt = 'You are an assistant to a biologist who has performed various analysis on gene, protein and phosphoprotein data related to tumor expression. Each result will be labeled with a section name. You should note similar gene symbols that appear in different sections of the analysis. Most importantly, please try to contrast and compare the results for the various clusters. Please be careful with information you write about genes or pathways in that there should be no incorrect or assumed information. Do not include a conclusion or header for the discussion section.'
+    system_prompt = 'You are an assistant to a biologist who has performed various analysis on gene, protein and phosphoprotein data related to tumor expression. Each result will be labeled with a section name. You should note similar gene symbols that appear in different sections of the analysis, but do not label that as a section, instead point it out in a paragraph at the end. Most importantly, please try to contrast and compare the results for the various clusters. Please be careful with information you write about genes or pathways in that there should be no incorrect or assumed information. Do not include a conclusion or header for the discussion section.'
     response = client.chat.completions.create(
             model='gpt-4-turbo',
             messages=[{'role': 'system', 'content': system_prompt},
@@ -251,3 +251,46 @@ def create_discussion(results: dict):
         )
     discussion = response.choices[0].message.content
     return discussion
+
+def plot_clustered_stacked(dfall, labels=None, title="multiple stacked bar plot",  H="/", **kwargs):
+    """Given a list of dataframes, with identical columns and index, create a clustered stacked bar plot. 
+labels is a list of the names of the dataframe, used for the legend
+title is a string for the title of the plot
+H is the hatch used for identification of the different dataframe"""
+
+    n_df = len(dfall)
+    n_col = len(dfall[0].columns) 
+    n_ind = len(dfall[0].index)
+    axe = plt.subplot(111)
+
+    for df in dfall : # for each data frame
+        axe = df.plot(kind="bar",
+                      linewidth=0,
+                      stacked=True,
+                      ax=axe,
+                      color=['coral', 'peachpuff', 'royalblue', 'lightblue'],
+                      legend=False,
+                      grid=False,
+                      **kwargs)  # make bar plots
+
+    h,l = axe.get_legend_handles_labels() # get the handles we want to modify
+    for i in range(0, n_df * n_col, n_col): # len(h) = n_col * n_df
+        for j, pa in enumerate(h[i:i+n_col]):
+            for rect in pa.patches: # for each index
+                rect.set_x(rect.get_x() + 1 / float(n_df + 1) * i / float(n_col))
+                rect.set_hatch(H * int(i / n_col)) #edited part     
+                rect.set_width(1 / float(n_df + 1))
+
+    axe.set_xticks((np.arange(0, 2 * n_ind, 2) + 1 / float(n_df + 1)) / 2.)
+    axe.set_xticklabels(df.index, rotation = 90)
+
+    # Add invisible data to add another legend
+    n=[]        
+    for i in range(n_df):
+        n.append(axe.bar(0, 0, color="gray", hatch=H * i))
+
+    l1 = axe.legend(h[:n_col], l[:n_col], loc=[1.01, 0.5])
+    if labels is not None:
+        l2 = plt.legend(n, labels, loc=[1.01, 0.1]) 
+    axe.add_artist(l1)
+    return axe
