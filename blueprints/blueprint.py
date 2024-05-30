383e4953be36796ca46265b4a91603b4d79761f7
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-05-30 11:05:30 -0400

    move/restyle buttons

--- blueprints/blueprint.py
+++ blueprints/blueprint.py
@@ -8,6 +8,8 @@ blueprint = Blueprint('blueprint', __name__)
 
 @blueprint.route('numruns')
 def numruns():
+  if 'APPYTER_DATA_DIR' not in os.environ or '#?key=' not in os.getenv('APPYTER_DATA_DIR') or '&secret=' not in os.getenv('APPYTER_DATA_DIR'):
+    return {'num_runs': 0}
   key = os.getenv('APPYTER_DATA_DIR').split('#?key=')[-1].split('&')[0]
   secret = os.getenv('APPYTER_DATA_DIR').split('&secret=')[-1]
   s3 = s3fs.S3FileSystem(key = key, secret = secret)
