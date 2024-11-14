e872afc4f460c4fede791c2b6f4a5fcc16cb5113
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-11-14 10:51:45 -0500

    fix counter

--- blueprints/blueprint.py
+++ blueprints/blueprint.py
@@ -10,7 +10,7 @@ def numruns():
   if 'APPYTER_DATA_DIR' not in os.environ or '#?key=' not in os.getenv('APPYTER_DATA_DIR') or '&secret=' not in os.getenv('APPYTER_DATA_DIR'):
     return {'num_runs': 0}
   key = os.getenv('APPYTER_DATA_DIR').split('#?key=')[-1].split('&')[0]
-  secret = os.getenv('APPYTER_DATA_DIR').split('&secret=')[-1]
+  secret = os.getenv('APPYTER_DATA_DIR').split('&secret=')[-1].replace('%2B', '+')
   s3 = s3fs.S3FileSystem(key = key, secret = secret)
   n_runs = len(s3.ls('s3://ct2targets/output'))
   response = make_response({'num_runs': n_runs})
