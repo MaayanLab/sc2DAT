3b68c538e3c08703ae928ecd9ce2e7cddcbda460
Daniel Clarke <u8sand@gmail.com>
2025-07-01 18:55:00 -0400

    Use appyter fsspec helpers to access APPYTER_DATA_DIR

--- blueprints/blueprint.py
+++ blueprints/blueprint.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, make_response
 import os
 import s3fs
+from appyter.ext.fsspec.core import url_to_chroot_fs
 from dotenv import load_dotenv
 load_dotenv()
 blueprint = Blueprint('blueprint', __name__)
 
 @blueprint.route('numruns')
 def numruns():
-  if 'APPYTER_DATA_DIR' not in os.environ or '#?key=' not in os.getenv('APPYTER_DATA_DIR') or '&secret=' not in os.getenv('APPYTER_DATA_DIR'):
-    return {'num_runs': 0}
-  key = os.getenv('APPYTER_DATA_DIR').split('#?key=')[-1].split('&')[0]
-  secret = os.getenv('APPYTER_DATA_DIR').split('&secret=')[-1].replace('%2B', '+')
-  s3 = s3fs.S3FileSystem(key = key, secret = secret)
-  n_runs = len(s3.ls('s3://ct2targets/output'))
+  fs = url_to_chroot_fs(os.environ.get('APPYTER_DATA_DIR'))
+  n_runs = len(fs.ls('output'))
   response = make_response({'num_runs': n_runs})
   response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
   response.headers['Pragma'] = 'no-cache'
